4940d5365b230a712512b24152d33e0d7f6da863
fredpell1 <fredericpelletier7@gmail.com>
2022-03-09 21:04:02 -0500

    refactor 2048-python for Ai inputs

--- AI/training.py
+++ AI/training.py
@@ -1,3 +1,4 @@
+import tkinter
 import neat
 import pickle 
 from pathlib import Path
@@ -10,6 +11,19 @@ sys.path.append(PARENTPATH + "/2048-python")
 
 import logic, puzzle, constants
 
+
+
+
+if __name__ == "__main__":
+    game = puzzle.GameGrid()
+    
+    running = True
+
+    while running:
+        try:
+            game.one_loop()
+        except tkinter.TclError:
+            running = False
 
 
 
